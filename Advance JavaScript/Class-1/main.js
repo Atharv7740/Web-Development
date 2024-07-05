99e1e4231ea11986a693edf76a7a8eadfc904945
Atharv7740 <atharv7740@gmail.com>
2024-07-05 19:55:03 +0530

    update

---
+++ Advance JavaScript/Class-1/main.js
@@ -0,0 +1,21 @@
+// const arr=[1,2,3,4,5,6,7,8,9,10]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%2==0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// console.log(arr);
+// Ques: 2
+
+var obj={
+    FirstName,
+    Gender,
+    Age
+}
+var a=new obj();
+a.FirstName="Atharv";
+a.age=20;
+a.gender="Male";
+
+console.log(a);
