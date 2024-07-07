56e3c0bdc739939baa21791ff4f4db611a656bbb
Atharv7740 <atharv7740@gmail.com>
2024-07-07 16:36:06 +0530

    update

--- Advance JavaScript/Class-1/main.js
+++ Advance JavaScript/Class-1/main.js
@@ -6,5 +6,64 @@
 // }
 
 // console.log(arr);
-// Ques: 2
+
+// if else  for loop code;
+
+
+
+// const firstName=["atharv","Mohit","Ram"];
+// const age=["20","19","17"];
+
+// for(let i=0;i<age.length;i++)
+// if(age[i]>18){
+//     console.log(firstName[i]);
+// }
+
+// arrays
+
+// const a=["Hello",1,(name)=>{console.log("hello "+name)},];
+// a[2]("Atharv");
+
+//object
+
+// const obj={
+//     firstName:"Atharv",
+//     age:20,
+//     gender:"Male"
+// }
+
+// console.log(obj["firstName"]);
+
+// Function
+
+// function sum(a, b){
+//     result=a+b;
+//     return result;
+
+// }
+
+// function displayResult(data){
+//     console.log("Result of sum: "+ data);
+
+// }
+
+// displayResult(sum(1,2));
+
+
+// function calback( passing function as a parameter inside fucntion )
+
+function calculate(a,b,operation){
+    const val= operation(a,b);
+    console.log(val);
+}
+
+function sum(a,b){
+    return a+b;
+
+}
+
+calculate(1,2,sum);
+
+
+
 
